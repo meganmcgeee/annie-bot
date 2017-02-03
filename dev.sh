db11bad0ff40a966bc033dd6acd6eff37fbfb307
Matthew Jesuele <matt@makeapps.io>
2017-02-03 13:12:51 -0800

    Updated build process to use Gulp

--- dev.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-
-./node_modules/.bin/babel --watch src -d lib -s &
-./node_modules/.bin/nodemon --ignore src lib/index.js
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const babel = require(`gulp-babel`);
+const gulp = require(`gulp`);
+// const env = require('gulp-env');
+const nodemon = require(`gulp-nodemon`);
+const plumber = require(`gulp-plumber`);
+const sourcemaps = require(`gulp-sourcemaps`);
+/* eslint-enable import/no-extraneous-dependencies */
+
+const babelOptions = {
+  presets: [
+    [`env`, {
+      targets: {
+        node: 7.5,
+      },
+    }],
+    `stage-3`,
+  ],
+};
+
+gulp.task(`js`, done => {
+  gulp.src(`src/**/*.js`)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(babel(babelOptions))
+    .pipe(sourcemaps.write(`.`))
+    .pipe(gulp.dest(`lib`))
+    .on(`end`, done);
+});
+
+gulp.task(`default`, [`js`], () => {
+  gulp.watch(`src/**/*.js`, [`js`]);
+  // env({ file: '.env.json' });
+  nodemon({ script: `lib/index.js`, ignore: `src` });
+});
