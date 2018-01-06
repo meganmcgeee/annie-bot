51d371bd9a4457b8161cef21df69493ec58bafb9
meganmcgeee <meganpmcgee@gmail.com>
2018-01-05 18:06:08 -0800

    Updated botbuilder framework. App still not running.

--- dist/applyMiddleware.js
+++ dist/applyMiddleware.js
@@ -1,24 +1,24 @@
-'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+
+Object.defineProperty(exports, `__esModule`, {
+  value: true,
 });
 
-var _showSessionStackTrace = require('./showSessionStackTrace');
+let _showSessionStackTrace = require(`./showSessionStackTrace`);
 
-var _showSessionStackTrace2 = _interopRequireDefault(_showSessionStackTrace);
+let _showSessionStackTrace2 = _interopRequireDefault(_showSessionStackTrace);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj } }
 
 const dumbassMiddleware = {
   botbuilder(session, next) {
     session.send(`test`);
     next();
-  }
+  },
 };
 
 exports.default = bot => {
   bot.use(_showSessionStackTrace2.default);
   bot.use(dumbassMiddleware);
 };
-//# sourceMappingURL=applyMiddleware.js.map
+// # sourceMappingURL=applyMiddleware.js.map
